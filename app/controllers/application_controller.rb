909eb6d23b1dc528c3f194e95b48b658907cdbdc
agdsarge <agdsargeant@gmail.com>
2020-05-13 17:56:56 -0400

    chasing green

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,22 +7,24 @@ class ApplicationController < Sinatra::Base
 
   # code actions here!
   get '/recipes' do
-
       @recipes = Recipe.all
-
       erb :index
   end
 
   get '/recipes/new' do
   end
 
-  get '/recipes/:id' do
-      @recipe = Recipe.find(params[:id])
-      erb :show
-  end
   get '/recipes/:id/edit' do
 
   end
 
+  get '/recipes/:id' do
+      @recipe = Recipe.find(params[:id])
+      erb :show
+  end
 
+  delete '/recipes/:id' do
+      Recipe.delete(params[:id])
+      redirect '/recipes'
+  end
 end
